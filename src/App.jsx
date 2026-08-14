3334d635c0f0bd6f6aca41d64c8aa7685ea21388
jenilraiyani <jenil6958@gmail.com>
2026-08-14 13:15:33 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -54,7 +54,7 @@ function App() {
     };
 
     try {
-      const response = await fetch('http://localhost:5000/predict', {
+      const response = await fetch('https://loan-default-model.onrender.com/predict', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(payload),
